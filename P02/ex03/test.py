484a3f243f38396a9f0fa67df84dc60038c9ba84
Joann ACHER <jacher@e1r7p16.42.fr>
2021-11-11 19:06:08 +0100

    P02 ex03

--- P02/ex03/test.py
+++ P02/ex03/test.py
@@ -1,29 +1,37 @@
 from csvreader import CsvReader
 
-if __name__ == "__main__":
-        with CsvReader('good.csv') as file:
-            print("\n******* GET DATA ********")
-            data = file.getdata()
-            print(data)
-            print("\n******* GET HEADER ********")
-            header = file.getheader()
-            print(header)
+try:
+    if __name__ == "__main__":
+            with CsvReader('good.csv') as file:
+                print("\n******* GET DATA ********")
+                data = file.getdata()
+                print(data)
+                print("\n******* GET HEADER ********")
+                header = file.getheader()
+                print(header)
 
-            file.header = True
-            file.skip_top = 2
-            file.skip_bottom = 5
-            print("\n******* GET DATA ********")
-            data = file.getdata()
-            print(data)
-            print("\n******* GET HEADER ********")
-            header = file.getheader()
-            print(header)
+                file.header = True
+                file.skip_top = 2
+                file.skip_bottom = 5
+                print("\n******* GET DATA ********")
+                data = file.getdata()
+                print(data)
+                print("\n******* GET HEADER ********")
+                header = file.getheader()
+                print(header)
 
-if __name__ == "__main__":
-    with CsvReader('bad.csv') as file:
-        print("\n******* DEUXIEME TEST ********")
+    print("\n******* DEUXIEME TEST ********")
+    if __name__ == "__main__":
+        with CsvReader('bad.csv') as file:
+            if file == None:
+                print("File is corrupted")
 
-        if file == None:
-            print("File is corrupted")
- 
+    print("\n******* TROISIEME TEST ********")
+    if __name__ == "__main__":
+        with CsvReader('dont_exist.csv') as file:
+            if file == None:
+                print("File is corrupted")
+
+except ValueError as err:
+    print(err.args)
 
