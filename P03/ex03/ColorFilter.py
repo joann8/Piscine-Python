ba487bb6db429e9e6fb30f2a2c4a567b1b3ba87f
Joann ACHER <jacher@e1r9p8.42.fr>
2021-11-16 19:26:51 +0100

    last commit before restart

--- P03/ex03/ColorFilter.py
+++ P03/ex03/ColorFilter.py
@@ -3,149 +3,145 @@ import copy
 
 class ColorFilter():
 
-    @staticmethod
-    def invert(array):
-        """
-        Inverts the color of the image received as a numpy array.
-        Args:
-            array: numpy.ndarray corresponding to the image.
-        Return:
-            array: numpy.ndarray corresponding to the transformed image.
-            None: otherwise.
-        Raises:
-            This function should not raise any Exception.
-        """
-        if not isinstance(array, np.ndarray):
-            print("This array is not an numpy array")
-            return None
-        return 1 - array[:,:,:3]
+	@staticmethod
+	def invert(array):
+		"""
+		Inverts the color of the array received as a numpy array.
+		Args:
+			array: numpy.ndarray corresponding to the array.
+		Return:
+			array: numpy.ndarray corresponding to the transformed array.
+			None: otherwise.
+		Raises:
+			This function should not raise any Exception.
+		"""
+		if not isinstance(array, np.ndarray):
+			print("This array is not an numpy array")
+			return None
+		return 1 - array[:,:,:3]
 
-    @staticmethod
-    def to_blue(array):
-        """
-        Applies a blue filter to the image received as a numpy array.
-        Args:
-            array: numpy.ndarray corresponding to the image.
-        Return:
-            array: numpy.ndarray corresponding to the transformed image.
-            None: otherwise.
-        Raises:
-            This function should not raise any Exception.
-        ◦ Authorized functions:.zeros,.shape,.dstack
-        ◦ Authorized operator:None
-        """
-        if not isinstance(array, np.ndarray):
-            print("This array is not an numpy array")
-            return None
-        blue_arr = np.zeros(np.shape(array[:,:,:3]))
-        blue_arr[:,:,2] = array[:,:,2]
-        return blue_arr
+	@staticmethod
+	def to_blue(array):
+		"""
+		Applies a blue filter to the array received as a numpy array.
+		Args:
+			array: numpy.ndarray corresponding to the array.
+		Return:
+			array: numpy.ndarray corresponding to the transformed array.
+			None: otherwise.
+		Raises:
+			This function should not raise any Exception.
+		◦ Authorized functions:.zeros,.shape,.dstack
+		◦ Authorized operator:None
+		"""
+		if not isinstance(array, np.ndarray):
+			print("This array is not an numpy array")
+			return None
+		blue_arr = np.zeros(np.shape(array[:,:,:3]))
+		blue_arr[:,:,2] = array[:,:,2]
+		return blue_arr
 
-    @staticmethod
-    def to_green(array):
-        """
-        Applies a green filter to the image received as a numpy array.
-        Args:
-            array: numpy.ndarray corresponding to the image.
-        Return:
-            array: numpy.ndarray corresponding to the transformed image.
-            None: otherwise.
-        Raises:
-            This function should not raise any Exception.
-        ◦ Authorized functions: copy. deepcopy 
-        ◦ Authorized operator: *
-        """
-        if not isinstance(array, np.ndarray):
-            print("This array is not an numpy array")
-            return None
-        return copy.deepcopy(array[:,:,:3] * [0, 1, 0])
-    
-    @staticmethod
-    def to_red(array): 
-        """
-        Applies a red filter to the image received as a numpy array.
-        Args:
-            array: numpy.ndarray corresponding to the image.
-        Return:
-            array: numpy.ndarray corresponding to the transformed image.
-            None: otherwise.
-        Raises:
-            This function should not raise any Exception.
-            Authorized functions:.to_green,.to_blue
-        """
-        if not isinstance(array, np.ndarray):
-            print("This array is not an numpy array")
-            return None
-        obj = ColorFilter()
-        return (array[:,:,:3]  - obj.to_blue(array) - obj.to_green(array))
+	@staticmethod
+	def to_green(array):
+		"""
+		Applies a green filter to the array received as a numpy array.
+		Args:
+			array: numpy.ndarray corresponding to the array.
+		Return:
+			array: numpy.ndarray corresponding to the transformed array.
+			None: otherwise.
+		Raises:
+			This function should not raise any Exception.
+		◦ Authorized functions: copy. deepcopy 
+		◦ Authorized operator: *
+		"""
+		if not isinstance(array, np.ndarray):
+			print("This array is not an numpy array")
+			return None
+		return copy.deepcopy(array[:,:,:3] * [0, 1, 0])
+	
+	@staticmethod
+	def to_red(array): 
+		"""
+		Applies a red filter to the array received as a numpy array.
+		Args:
+			array: numpy.ndarray corresponding to the array.
+		Return:
+			array: numpy.ndarray corresponding to the transformed array.
+			None: otherwise.
+		Raises:
+			This function should not raise any Exception.
+			Authorized functions:.to_green,.to_blue
+		"""
+		if not isinstance(array, np.ndarray):
+			print("This array is not an numpy array")
+			return None
+		obj = ColorFilter()
+		return (array[:,:,:3]  - obj.to_blue(array) - obj.to_green(array))
 
-    @staticmethod
-    def to_celluloid(array): 
-        """
-        Applies a celluloid filter to the image received as a numpy array.
-        Celluloid filter must display at least four thresholds of shades.
-        Be careful! You are not asked to apply black contour on the object,
-        you only have to work on the shades of your images.
-        Remarks:
-            celluloid filter is also known as cel-shading or toon-shading.
-        Args:
-            array: numpy.ndarray corresponding to the image.
-        Return:
-            array: numpy.ndarray corresponding to the transformed image.
-            None: otherwise.
-        Raises:
-            This function should not raise any Exception.
-            Authorized functions:.arange,.linspace
-     """
-        if not isinstance(array, np.ndarray):
-            print("This array is not an numpy array")
-            return None
-        interval = np.linspace(0, 1, 5)
-        for i in np.arange(1, 5):
-            array[(array>=interval[i - 1]) & (array<interval[i])] = interval[i - 1]
-        return array
+	@staticmethod
+	def to_celluloid(array): 
+		"""
+		Applies a celluloid filter to the array received as a numpy array.
+		Celluloid filter must display at least four thresholds of shades.
+		Be careful! You are not asked to apply black contour on the object,
+		you only have to work on the shades of your arrays.
+		Remarks:
+			celluloid filter is also known as cel-shading or toon-shading.
+		Args:
+			array: numpy.ndarray corresponding to the array.
+		Return:
+			array: numpy.ndarray corresponding to the transformed array.
+			None: otherwise.
+		Raises:
+			This function should not raise any Exception.
+			Authorized functions:.arange,.linspace
+	 """
+		if not isinstance(array, np.ndarray):
+			print("This array is not an numpy array")
+			return None
+		interval = np.linspace(0, 1, 5)
+		for i in np.arange(1, 5):
+			array[(array>=interval[i - 1]) & (array<interval[i])] = interval[i - 1]
+		return array
 
-    @staticmethod
-    def to_grayscale(array, filter, weight = None): #proto change pour l'exemple
-        """
-        Applies a grayscale filter to the image received as a numpy array.
-        For filter = ’mean’/’m’: performs the mean of RBG channels.
-        For filter = ’weight’/’w’: performs a weighted mean of RBG channels.
-        Args:
-            array: numpy.ndarray corresponding to the image.
-            filter: string with accepted values in [’m’,’mean’,’w’,’weight’]
-            weights: [kwargs] list of 3 floats where the sum equals to 1,
-                    corresponding to the weights of each RBG channels.
-        Return:
-            array: numpy.ndarray corresponding to the transformed image.
-            None: otherwise.
-        Raises:
-            This function should not raise any Exception.
-            ◦ Authorizedfunctions:.sum,.shape,.tile,.reshape,.dstack,.broadcast_to,.as_type 
-            ◦ Authorized operator:*,/
+	@staticmethod
+	def to_grayscale(array, filter, weight = None): #proto change pour l'exemple
+		"""
+		Applies a grayscale filter to the array received as a numpy array.
+		For filter = ’mean’/’m’: performs the mean of RBG channels.
+		For filter = ’weight’/’w’: performs a weighted mean of RBG channels.
+		Args:
+			array: numpy.ndarray corresponding to the array.
+			filter: string with accepted values in [’m’,’mean’,’w’,’weight’]
+			weights: [kwargs] list of 3 floats where the sum equals to 1,
+					corresponding to the weights of each RBG channels.
+		Return:
+			array: numpy.ndarray corresponding to the transformed array.
+			None: otherwise.
+		Raises:
+			This function should not raise any Exception.
+			◦ Authorizedfunctions:.sum,.shape,.tile,.reshape,.dstack,.broadcast_to,.as_type 
+			◦ Authorized operator:*,/
 
-         """
-        #check types
-        if not isinstance(filter, str) or not isinstance(array, np.ndarray):
-            print("Wrong types inputs")
-            return None
-        if filter not in['m', 'mean', 'w', 'weight']:
-            print("This filter inputs is not correct ")
-            return None
-        if weight is not None:
-            if not isinstance(weight,list) or len(weight) != 3 and not isinstance(weight[0], float) or not isinstance(weight[1], float) or not isinstance(weight[2], float) or weight[0] + weight[1] + weight[2] != 1:
-                print("Need 3 floats with sum = 1")
-                return None
-            else:
-                r = weight[0]
-                g = weight[1]
-                b = weight[2]
-        else:
-            r = 0.2126 #0.2989 
-            g = 0.7152 #0.5870
-            b = 0.0722 #0.1140 
-    
-        
-        gray = array[:,:,0] * r  + array[:,:,1] * 255 * g + array[:,:,2] * 255 * b
-        return gray
-       
+		 """
+		#check types
+		if not isinstance(filter, str) or not isinstance(array, np.ndarray):
+			print("Wrong types inputs")
+			return None
+		if filter not in['m', 'mean', 'w', 'weight']:
+			print("This filter inputs is not correct ")
+			return None
+		if weight is not None:
+			if not isinstance(weight,list) or len(weight) != 3 and not isinstance(weight[0], float) or not isinstance(weight[1], float) or not isinstance(weight[2], float) or weight[0] + weight[1] + weight[2] != 1:
+				print("Need 3 floats with sum = 1")
+				return None
+			else:
+				tmp = array[:,:,:3]
+				means = np.sum(tmp * np.broadcast_to(weight, tmp.shape), -1) / 3
+				return np.dstack((means, means, means))
+		else:
+			tmp = array[:,:,:3]
+			print(tmp)
+			print(np.sum(tmp, -1))
+			return np.dstack((np.sum(tmp, -1), np.sum(tmp, -1), np.sum(tmp, -1)))/ 3
